b0524c5704c796238b00cf46b170c2cb9af00d43
stevescilar <stevemuambi@gmail.com>
2023-07-28 16:31:57 +0300

    edited about admin  page

--- resources/views/admin/about/all_images.blade.php
+++ resources/views/admin/about/all_images.blade.php
@@ -10,12 +10,12 @@
                         <div class="row">
                             <div class="col-12">
                                 <div class="page-title-box d-sm-flex align-items-center justify-content-between">
-                                    <h4 class="mb-sm-0">Data Tables</h4>
+                                    <h4 class="mb-sm-0">Tools of Work</h4>
 
                                     <div class="page-title-right">
                                         <ol class="breadcrumb m-0">
-                                            <li class="breadcrumb-item"><a href="javascript: void(0);">Tables</a></li>
-                                            <li class="breadcrumb-item active">Data Tables</li>
+                                            <li class="breadcrumb-item"><a href="javascript: void(0);">My Tools</a></li>
+                                            <li class="breadcrumb-item active">All</li>
                                         </ol>
                                     </div>
 
